df9b9790b91f288038cb30fd3a24b3898f0d0c16
nick5595 <nick55955@gmail.com>
2024-03-20 10:15:11 +0100

    changed parentCommentId: string to childComments: string[]

--- blogserver/src/comment/DTO/comment.dto.ts
+++ blogserver/src/comment/DTO/comment.dto.ts
@@ -2,5 +2,5 @@ export class CommentDTO {
   readonly _id: string;
   readonly author: string;
   readonly content: string;
-  readonly parentCommentId: string;
+  readonly childComments: string[];
 }
